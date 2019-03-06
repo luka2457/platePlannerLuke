f26901a62fcdcbf4849d9973ed24013d82cc60e6
Luke <luke.karlovich@colorado.edu>
2019-03-05 21:56:53 -0700

    added facebook Oauth

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
+var passport = require("passport");
+var FacebookStrategy = require('passport-facebook')
+
 var express = require("express");
 var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 8000;
 
 var app = express();
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: '2812611205629771',
+      clientSecret: 'debd2a021c974b66ca2c5736dd15e77d',
+      callbackURL: "https://efd8e699.ngrok.io/auth/facebook/callback"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(null, profile);
+      // });
+    }
+  )
+);
+
+app.use(passport.initialize());
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { session: false }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    //res.redirect('/');
+    res.send('auth was good');
+  });
+
+
+
+
+
 
 var db = require("./models");
 
