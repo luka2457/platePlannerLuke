f166b712aac1bb327318ae7acaa6c3ab1b211b38
Luke <luke.karlovich@colorado.edu>
2019-04-22 19:50:12 -0600

    hide FB client info

--- keys.js
+++ keys.js
@@ -1,5 +1,14 @@
-require('dotenv').config({ path: 'process.env' });
+require("dotenv").config({ path: "process.env" });
 
-var key = [process.env.KEY, process.env.KEY1, process.env.KEY2, process.env.KEY3, process.env.KEY4, process.env.KEY5, process.env.KEY6, process.env.KEY7]
+var key = [
+  process.env.KEY,
+  process.env.KEY1,
+  process.env.KEY2,
+  process.env.KEY3,
+  process.env.KEY4,
+  process.env.KEY5,
+  process.env.KEY6,
+  process.env.KEY7
+];
 
 module.exports = key;
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-var passport = require("passport");
-var FacebookStrategy = require('passport-facebook')
+//pull in CLIENTID AND CLIENTSECRET from process.env file
+require("dotenv").config({ path: "process.env" });
 
+var passport = require("passport");
+var FacebookStrategy = require("passport-facebook");
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 8000;
-
+var PORT = process.env.PORT || 3000;
 var app = express();
+var db = require("./models");
+// Set Handlebars
+var exphbs = require("express-handlebars");
 
+//Facebook setup for passport
 passport.use(
   new FacebookStrategy(
     {
-      clientID: '2812611205629771',
-      clientSecret: 'debd2a021c974b66ca2c5736dd15e77d',
+      clientID: process.env.CLIENTID,
+      clientSecret: process.env.CLIENTSECRET,
       callbackURL: "https://plateplanner.herokuapp.com/auth/facebook/callback",
       //Start ngrok and use their provide url .../auth/facebook/callback when not deployed
-      // callbackURL: "https://efd8e699.ngrok.io/auth/facebook/callback",
-      profileFields: ['id', 'displayName', 'emails', 'picture.width(200).height(200)']
+      // callbackURL: "https://be209c6f.ngrok.io/auth/facebook/callback",
+      profileFields: [
+        "id",
+        "displayName",
+        "emails",
+        "picture.width(200).height(200)"
+      ]
     },
-    function (accessToken, refreshToken, profile, cb) {
-        return cb(null, profile);
+    function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
     }
   )
 );
-
+//Initialize Facebook
 app.use(passport.initialize());
-
-var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -37,21 +44,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
 
 require("./routes/routes.js")(app);
 
 // Start our server so that it can begin listening to client requests.
-//if force is true it will drop whatever table first and re-create it afterwards
-//false however will keep whatever was already there
-db.sequelize.sync({ force: false }).then(function () {
-  app.listen(PORT, function () {
-    // Log (server-side) when our server has started
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
   });
 });
